b28a30d7f9e540052161202be6783779ad10ebca
Vijay Dev <vijaydev.in@gmail.com>
2018-03-18 10:50:04 +0530

    added a mock fetch layer

---
+++ src/mockFetch/db.js
@@ -0,0 +1,91 @@
+export const data = [{
+    "id": "120000",
+    "firstname": "Mathias",
+    "lastname": "Raedler",
+    "avatarUrl": "",
+    "email": "mathias@raedler.neumail.com",
+    "phone": "(032) 5343-5532",
+    "hasPremium": false,
+    "bids": []
+}, {
+    "id": "120001",
+    "firstname": "Karlheinz",
+    "lastname": "Apfelbaum",
+    "avatarUrl": "",
+    "email": "karlheinz@apfelbaum.neumail.com",
+    "phone": "(032) 6433-7323",
+    "hasPremium": true,
+    "bids": []
+}, {
+    "id": "120003",
+    "firstname": "Benedikt",
+    "lastname": "Deutsch",
+    "avatarUrl": "",
+    "email": "benedikt@deutsch.neumail.com",
+    "phone": "(032) 5432-8223",
+    "hasPremium": false,
+    "bids": []
+}, {
+    "id": "120004",
+    "firstname": "Konrad",
+    "lastname": "Engels",
+    "avatarUrl": "",
+    "email": "konrad@engels.neumail.com",
+    "phone": "(034) 5432-8123",
+    "hasPremium": false,
+    "bids": []
+}, {
+    "id": "120005",
+    "firstname": "Tilman",
+    "lastname": "Holdermann",
+    "avatarUrl": "",
+    "email": "tilman@holdermann.neumail.com",
+    "phone": "(034) 5322-8123",
+    "hasPremium": true,
+    "bids": []
+}, {
+    "id": "120006",
+    "firstname": "Janine",
+    "lastname": "Jonas",
+    "avatarUrl": "",
+    "email": "janine@jonas.neumail.com",
+    "phone": "(035) 3232-7843",
+    "hasPremium": false,
+    "bids": []
+}, {
+    "id": "120007",
+    "firstname": "Kunz",
+    "lastname": "Schenk",
+    "avatarUrl": "",
+    "email": "kunz@schenk.neumail.com",
+    "phone": "(035) 6323-7242",
+    "hasPremium": false,
+    "bids": []
+}, {
+    "id": "120008",
+    "firstname": "Sybille",
+    "lastname": "Kiesling",
+    "avatarUrl": "",
+    "email": "sybille@kiesling.neumail.com",
+    "phone": "(012) 6323-3436",
+    "hasPremium": true,
+    "bids": []
+}, {
+    "id": "120009",
+    "firstname": "Jaqueline",
+    "lastname": "Waldmüller",
+    "avatarUrl": "",
+    "email": "jaqueline@waldmüller.neumail.com",
+    "phone": "(012) 3422-5632",
+    "hasPremium": true,
+    "bids": []
+}, {
+    "id": "120010",
+    "firstname": "Edgar",
+    "lastname": "Hohenlohe",
+    "avatarUrl": "",
+    "email": "Edgar@hohenlohe.neumail.com",
+    "phone": "(022) 2232-6633",
+    "hasPremium": false,
+    "bids": []
+}]
---
+++ src/mockFetch/index.js
@@ -0,0 +1,141 @@
+import data from './db'
+
+export class MockFetch {
+    constructor() {
+        this.body = new ReadableStream()
+        this.bodyUsed = false
+        this.headers = new Headers()
+        this.ok = true
+        this.redirected = false
+        this.status = 200
+        this.statusText = 'ok'
+        this.type = 'basic'
+        this.url = ''
+
+        this._options = {}
+        this._data = data
+
+        return this
+    }
+
+    fetch(url, options = {}) {
+        this.url = url
+        this._options = options
+        
+        return new Promise(resolve => {
+            setTimeout(() => {
+                resolve(this)
+            }, 300)
+        })
+    }
+
+    json() {
+        const getMerchantList = this.url.indexOf('/merchant?') === 0
+        const merchantDetails = this.url.indexOf('/merchant/') === 0
+
+        if (getMerchantList) {
+            return this._getMerchantList()
+        } else if (merchantDetails) {
+            return this._merchantDetails()
+        }
+
+        return {}
+    }
+
+    _getMerchantList() {
+        const paramPart = this.url.slice(this.url.indexOf('?') + 1)
+        const paramAry = paramPart.split('&')
+        let pageNo = 0
+        let perPage = 10
+
+        paramAry.forEach(param => {
+            const parts = param.split('=')
+            const key = parts[0]
+            const value = parseInt(parts[1], 10)
+
+            if (!isNaN(value)) {
+                if (key === 'page_no') {
+                    pageNo = value > 0 ? value - 1 : 0
+                } else if (key === 'per_page') {
+                    perPage = value <= 0 ? 1 : value
+                }
+            }
+        })
+
+        return this._data.slice(pageNo, perPage)
+    }
+
+    _merchantDetails() {
+        const method = this._options.method.toLowerCase()
+        const id = this.url.split('/merchant/')[1]
+        const idInt = parseInt(id, 10)
+
+        if (method === 'post') {
+            return this._onPost()
+        } else if (method === 'put' || method === 'patch') {
+            return this._onPutPatch(idInt)
+        }
+
+        return this._onGet(idInt)
+    }
+
+    _onPost() {
+        const body = JSON.parse(this._options.body)
+        const lastId = this._data[this._data.length].id
+        const lastIdInt = parseInt(lastId, 10)
+        const id = lastIdInt + 1
+
+        this._data.push({
+            ...body,
+            id
+        })
+
+        return {
+            id,
+            message: 'Saved successfully.'
+        }
+    }
+
+    _onPutPatch(idInt) {
+        const body = JSON.parse(this._options.body)
+            
+        if (!isNaN(idInt)) {
+            const idStr = idInt.toString()
+            const foundIndex = this._data.findIndex(({ id }) => id === idStr)
+
+            if (foundIndex > -1) {
+                this._data[foundIndex] = {
+                    ...this._data[foundIndex],
+                    ...body
+                }
+
+                return {
+                    id: idStr,
+                    message: 'Updated successfully.'
+                }
+            }
+        }
+
+        return {
+            id: idInt,
+            message: 'Record not found.'
+        }
+    }
+
+    _onGet(idInt) {
+        const idStr = idInt.toString()
+        const found = this._data.find(({ id }) => id === idStr)
+
+        if (!!found) {
+            return found
+        }
+
+        return {}
+    }
+}
+
+const mockFetch = new MockFetch()
+const fetch = mockFetch.fetch
+
+export { mockFetch }
+export default fetch

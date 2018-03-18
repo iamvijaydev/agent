66956556ed6afcf4e1ab1828c43cdc0fdcb25561
Vijay Dev <vijaydev.in@gmail.com>
2018-03-18 07:05:28 +0530

    Added a route loading component

---
+++ src/components/Loading/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Loading = ({
+    error,
+    timedOut,
+    pastDelay
+}) => {
+    if (error) {
+        return <div>Error!</div>;
+    } else if (timedOut) {
+        return <div>Taking a long time...</div>;
+    } else if (pastDelay) {
+        return <div>Loading...</div>;
+    } else {
+        return null;
+    }
+}
